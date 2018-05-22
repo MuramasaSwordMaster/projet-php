0478cf5872c08dc9997068cafa33fdf3eb409e85
MuramasaSwordMaster <aurelien.depeyrelongue@gmail.com>
2018-05-22 17:22:45 +0200

    premiere page
    index

---
+++ inclusions/nav.inc.php
@@ -0,0 +1,28 @@
+ <nav id="nav-principale" class="navbar navbar-expand-md navbar-light bg-faded nav-principale">
+            <a class="navbar-brand" href="javascript:void(0)">Paris Nanterre</a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="search-container">
+                    <form action="/action_page.php">
+                        <input id="recherche" placeholder=" Rechercher un membre" name="search" type="text"><button type="submit"><svg class="svg-inline--fa fa-search fa-w-16" aria-hidden="true" data-prefix="fa" data-icon="search" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path></svg><!-- <i class="fa fa-search"></i> --></button>
+                    </form>
+                          </div>
+                        <div id="nom-nav-principale">
+                                    Bonjour William Pasco
+                                    <img id="photo-profil-nav-principale" src="http://localhost/projet-laravel-social/public/img/william.jpg">
+                        </div>
+                 <div class="nav-item  " style="position :absolute; right:180px;">
+                     <a href="./connexion" class="btn btn-light">Se connecter</a>
+                 </div>
+                <div class="nav-item  " style="position :absolute; right:20px;">
+                    <a href="#" class="btn btn-light">Se déconnecter</a>
+                </div>
+            </div>
+          </nav>
+        
+            
+           
+
+        
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Accueil reseau</title>
+    <?php 
+ include('inclusions/head-content.inc.php');
+ ?>
+
+</head>
+<body>
+<div id="wrapper">
+ <div class="container-fluid">
+
+ <?php 
+ 
+ include('inclusions/nav.inc.php');
+ 
+ ?>
+
+
+
+
+<div id="corps-conteneur">
+
+
+<div class="container-fluid">
+<div class="espaceur">
+
+</div>
+</div>
+    
+</div>
+</div>
+</div>
+
+</body>
+</html>

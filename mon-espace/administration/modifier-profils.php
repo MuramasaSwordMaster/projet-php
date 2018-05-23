27549ed2bbbb74b736ebaafc49f6a979ebb310cd
MuramasaSwordMaster <aurelien.depeyrelongue@gmail.com>
2018-05-23 20:25:14 +0200

    sdf
    sdf

--- mon-espace/administration/modifier-profils.php
+++ mon-espace/administration/modifier-profils.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Accueil reseau</title>
+    <title>mon compte</title>
     <?php 
-     include('../../inclusions/config.inc.php');
- include('../../inclusions/head-content.inc.php');
+     include('../inclusions/config.inc.php');
+ include('../inclusions/head-content.inc.php');
  ?>
 
 </head>
@@ -29,57 +29,29 @@
 
 <div class="container-fluid">
     <div class="row">
-        <div class="col-md-3">
+        <div class="col-md-12">
                 <div class="card" style="width: 100%;">
                         <div class="card-header">
-                          Paramètres de compte
+                          Modififications des profils
                         </div>
-                        <ul class="list-group list-group-flush">
-                                <li class="list-group-item"><a href="generaux">Généraux</a></li>
-                                <li class="list-group-item"><a href="a-propos">A propos</a></li>
-                                <li class="list-group-item"><a href="confidentialite">Confidentialité</a></li>
-                                <li class="list-group-item"><a href="mot-de-passe">Mot de passe</a></li>
-                                <li class="list-group-item"><a href="avatar">Avatar</a></li>
                         
-                                </ul>
-            </div>
-        </div>
-        <div class="col-md-9">
-                <div class="card" style="width: 100%;">
-                        <div class="card-header">
-                         Mot de passe
-                        </div>
-                       <div class="card-body">
-                <form action="#">
-                <div class="form-group">
-                <div class="row">
-                <div class="col-6">
-                    <label for="">Mot de passe courrant</label>
-                <input type="text" class="form-control" placeholder="Mot de passe">
-                </div>
-                <div class="col-6">
-                    <label for="">Nouveau mot de passe :</label>
-                  <input type="text" class="form-control" placeholder="Nouveau mot de passe">
-                </div>
-            </div>
-        </div>
-       
-        
-            <div class="form-group">
-                    <div class="row">
-                       
-                <div class="offset-6  col-6">
-                        <label for="">Répétez votre mot de passe :</label>
-                      <input type="text" class="form-control" placeholder="Répeter mot de passe">
-                    </div>
-
-                    
-                
-                </div>
-                </div>
-     
+                        
+                        
+                        <div class="">
                           
-                </form>
+                          <form class="" action="index.html" method="post">
+                            
+                            
+                            
+                            
+                            
+                          </form>
+                          
+                        </div>
+                    
+            </div>
+        </div>
+      
                        </div>
                     
                 </div>
@@ -99,7 +71,7 @@
 
  <?php 
  
- include('../../inclusions/js.inc.php');
+ include('../inclusions/js.inc.php');
  ?>
 </body>
 </html>

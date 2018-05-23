0f5139dec6be122cd449471327d0906d67215209
Sonson03 <34303018+Sonson03@users.noreply.github.com>
2018-05-23 15:13:58 +0200

    creation Bd

--- db/db.ini.php
+++ db/db.ini.php
@@ -1,14 +1,11 @@
 
 <?php
-/**
- * Simple example of extending the SQLite3 class and changing the __construct
- * parameters, then using the open method to initialize the DB.
- */
+/** using the open method to initialize the DB.*/
 class MyDB extends SQLite3
 {
     function __construct()
     {
-        $this->open('mysqlitedb.db');
+        $this->open('DBprojet.db');
     }
 }
 

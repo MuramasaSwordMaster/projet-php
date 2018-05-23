40df78a3fbdabbb49e7783b1b118f91309b1d829
Sonson03 <34303018+Sonson03@users.noreply.github.com>
2018-05-23 20:29:11 +0200

    readme

--- authentification/traitement/traitement-insc.php
+++ authentification/traitement/traitement-insc.php
@@ -14,24 +14,36 @@ $passe = $_POST['passe'];
 $email = $_POST['email'];
 $dateNaiss = $_POST['dateNaiss'];
 $adresse = $_POST['adresse'];
+<<<<<<< HEAD
 $mode = $_POST['mode'];
 /*$niveau = $_POST['niveau'];
 
 $statut = $_POST['statut'];*/
+=======
+$statut = $_POST['statut'];
+$niveau = $_POST['niveau'];
+$mode = $_POST['mode'];
+>>>>>>> 63901ce077dfa4330d395f40d44abf6fdce7298f
 
 
     $db->exec("INSERT INTO 
-    Personne (Id_Pers,Nom,Prenom,PWD,Mail,Date_Naissance,Adresse,Pic_src,Apropos) 
-    VALUES ('$num','$nom', '$prenom', '$passe', '$email', '$dateNaiss', '$adresse', '0', '0')");
+    Personne (Id_Pers,Nom,Prenom,PWD,Mail,Date_Naissance,Adresse,Pic_src,Apropos, Statut) 
+    VALUES ('$num','$nom', '$prenom', '$passe', '$email', '$dateNaiss', '$adresse', '0', '0', '$statut')");
   /*  $db->exec("CREATE OR REPLACE TRIGGER Remplir_etudie AFTER INSERT ON Personne 
     WHEN '$statut' = '2'
     BEGIN    
         INSERT INTO Etudie(Id_Pers, Annee, Note, Niveau, Mode, Num_interne) 
         VALUES ('$num', '2018', null, '$niveau', '$mode', '$num');
     END;");*/
+<<<<<<< HEAD
   /*  $db->exec("INSERT INTO Etudie(Id_Pers, Annee, Note, Niveau, Mode, Num_interne) 
         VALUES (99, '2018', null, '$niveau', '$mode', '$num');");*/
    echo($mode) ; 
+=======
+    $db->exec("INSERT INTO Etudie(Id_Pers, Annee, Note, Niveau, Mode, Num_interne) 
+        VALUES ('$num', '2018', null,   '$niveau', '$mode', '$num');");
+
+>>>>>>> 63901ce077dfa4330d395f40d44abf6fdce7298f
   
 }
 ?>
